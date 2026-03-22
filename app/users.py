d434445cad11f411d0273f781c5c1f195e79b96e
willdrewxdd <enduwilliam02@gmail.com>
2026-03-22 23:25:06 +0700

    Initial commit: FastAPI with ImageKit and Streamlit

---
+++ app/users.py
@@ -0,0 +1,43 @@
+import uuid
+from typing import Optional
+from fastapi import Depends, Request
+from fastapi_users import BaseUserManager, FastAPIUsers, UUIDIDMixin, models
+from fastapi_users.authentication import (
+    AuthenticationBackend,
+    BearerTransport,
+    JWTStrategy
+)
+from fastapi_users.db import SQLAlchemyUserDatabase
+from app.db import User, get_user_db
+
+SECRET = "1FDVpsCBP]~cU%piTZ"
+
+class UserManager(UUIDIDMixin, BaseUserManager[User, uuid.UUID]):
+    reset_password_token_secret = SECRET
+    verification_token_secret = SECRET
+
+    async def on_after_register(self, user, request = None):
+        return await super().on_after_register(user, request)
+    
+    async def on_after_forgot_password(self, user, token, request = None):
+        return await super().on_after_forgot_password(user, token, request)
+    
+    async def on_after_request_verify(self, user, token, request = None):
+        return await super().on_after_request_verify(user, token, request)
+
+async def get_user_manager(user_db: SQLAlchemyUserDatabase = Depends(get_user_db)):
+    yield UserManager(user_db)
+
+bearer_transport = BearerTransport(tokenUrl="auth/jwt/login")
+
+def get_jwt_strategy():
+    return JWTStrategy(secret=SECRET, lifetime_seconds=3600)
+
+auth_backend = AuthenticationBackend(
+    name="jwt",
+    transport=bearer_transport,
+    get_strategy=get_jwt_strategy,
+)
+
+fastapi_users = FastAPIUsers[User,uuid.UUID](get_user_manager, auth_backends=[auth_backend])
+current_active_user = fastapi_users.current_user(active=True)
