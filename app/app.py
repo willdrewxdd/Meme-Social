99577c0fa77e143233510d7241d24a36b5fbcadf
willdrewxdd <enduwilliam02@gmail.com>
2026-03-26 13:17:22 +0700

    Update app.py
    adding home route

--- app/app.py
+++ app/app.py
@@ -27,6 +27,10 @@ app.include_router(fastapi_users.get_reset_password_router(), prefix="/auth", ta
 app.include_router(fastapi_users.get_verify_router(UserRead), prefix="/auth", tags=["auth"])
 app.include_router(fastapi_users.get_users_router(UserRead, UserUpdate), prefix="/users", tags=["users"])
 
+@app.get("/")
+async def root():
+    return {"message": "API is running! Go to /docs for documentation."}
+
 
 @app.post("/upload")
 async def upload_file(
